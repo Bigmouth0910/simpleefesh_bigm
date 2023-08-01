a3eefd44490a1cdd6a220eebeb9b99873c314f6d
Bigmouth0910 <138143046+Bigmouth0910@users.noreply.github.com>
2023-08-01 18:06:13 +0800

    Update server.js add space
    add space

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // This is your test secret API key.
-const stripe = require('stripe')('sk_test_51NXc1kDNINKtzg8u6jK33b5KqOPLsspCx39afdNNX5EyKVVmdeNDpK5Tl5atlx0z5XLCAlqMX33IIRMKNAQsdvmF00IWJKN59b');
+const stripe = require('stripe')('sk_test_51NXc1kDNINKtzg8u6jK33b5KqOPLsspCx39afdNNX5EyKVVmdeNDpK5Tl5atlx0z5XLCAlqMX33IIRMKNAQsdvmF00IWJKN59b');      
 const express = require('express');
 const path = require('path');
 

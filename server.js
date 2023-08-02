389bc547e487fc0d8eb444449decf9fbe7f36de6
Bigmouth0910 <138143046+Bigmouth0910@users.noreply.github.com>
2023-08-02 18:11:10 +0800

    Update server.js

--- server.js
+++ server.js
@@ -56,8 +56,9 @@ app.get('/mypay', (req, res) => {
 app.get('/success', (req, res) => {
   // Get the absolute path of the HTML file    
   var data = { price: unitAmount };
+  const htmlPath = path.join(__dirname, 'checkout.html');
   // Send the HTML file as a response
-  res.render('success_new', { price: data.price });
+  res.sendFile(htmlPath);
 
 });
 
